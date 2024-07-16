b0f32f78f212523bdbcd7a144cee7fcd9ba8bf11
KKL <karol.kliniewski.ext@zeiss.com>
2024-07-16 22:30:14 +0200

    changes

--- selenium-tests/src/test/java/starter/StepDefinitions.java
+++ selenium-tests/src/test/java/starter/StepDefinitions.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,16 +21,29 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class StepDefinitions {
     private WebDriver driver;
     private WebDriverWait wait;
 
+
+    public void execJS(String script, Object... args){
+        ((JavascriptExecutor) driver).executeScript(script, args);
+    }
+
+    public void moveElementIntoView(WebElement element){
+        execJS("arguments[0].scrollIntoView();", element);
+        
+    }
     @Before
     public void setUp() throws MalformedURLException {
+        System.out.println("Starting RemoteWebDriver");
         EdgeOptions options = new EdgeOptions();
+        options.addArguments("remote-allow-or","test-type", "ignore-certificate-errors",
+        "incognito", "disable-infobars", "disable-gpu", "disable-default-apps", "disable-popup-blocking","start-maximized", "disable-dev-shm-usage", "--disable-dev-shm-usage", "disable-web-security", "disable-translate");
         driver = new RemoteWebDriver(new URL("http://selenium-grid-selenium-hub.selenium-grid/wd/hub"), options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Ustawienie oczekiwania na maksymalnie 10 sekund
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
     }
 
     @Given("I open the browser and go to the website")
@@ -39,46 +53,102 @@ public class StepDefinitions {
 
    @When("I click on the submit button")
     public void i_click_on_the_submit_button() {
-        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("FSsubmit")));
+        System.out.println("I click on the submit butto");
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"HTML1\"]/div[1]/table/tbody/tr[2]/td[2]")));
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+        }
         submitButton.click();
     }
 
     @Then("I should see {string}")
     public void i_should_see(String expectedMessage) {
-        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > div > div:nth-child(1) > div > h1")));
+        try {
+            Thread.sleep(20000);
+        } catch (Exception e) {
+        }
+        System.out.println("I should see " + expectedMessage);
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"HTML1\"]/div[1]/table/tbody/tr[2]/td[1]")));
         assertEquals(expectedMessage, errorMessage.getText());
+        List<WebElement> elementy = driver.findElements(By.xpath("//*[@id=\"HTML1\"]/div[1]/table/tbody/tr[2]/td"));
+        assertEquals(elementy.size(), 4);
     }
 
     @When("I fill out the form with:")
     public void i_fill_out_the_form_with(List<List<String>> formData) {
-        WebElement nameInput = driver.findElement(By.id("name"));
+        try {
+            Thread.sleep(22000);
+        } catch (Exception e) {
+        }
+        System.out.println("Starting filling inputs");
+        WebElement nameInput = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[1]/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[1]/input[@id=\"name\" and not (@hidden)]"));
+        System.out.println("Printing tagname " + nameInput.getTagName() + nameInput.isDisplayed());
+        
         nameInput.clear();
-        nameInput.sendKeys(formData.get(0).get(1)); // Sprawdź, czy poprawnie odwołujesz się do danych w liście formData
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+        }
+        nameInput.sendKeys(formData.get(0).get(1)); 
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+        }
+       
+        nameInput.click();
+        moveElementIntoView(nameInput);
 
-        WebElement emailInput = driver.findElement(By.id("email"));
+        WebElement emailInput = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[1]/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[1]/input[@id=\"email\" and not (@hidden)]"));
+        System.out.println("Printing tagname " + emailInput.getTagName() + emailInput.isDisplayed());
         emailInput.clear();
         emailInput.sendKeys(formData.get(1).get(1));
+        for (int i = 0; i < 20; i++) {
+                try {
+                    emailInput.click();
+                } catch (Exception e) {
+                    System.out.println("test");
+                }
+            
+        }
+        for (int i = 0; i < 12; i++) {
+            
+            moveElementIntoView(emailInput);
+        }
+        
 
-        WebElement phoneInput = driver.findElement(By.id("phone"));
+        
+
+        WebElement phoneInput = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[1]/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[1]/input[3]"));
+        System.out.println("Printing tagname " + phoneInput.getTagName() + phoneInput.isDisplayed());
         phoneInput.clear();
         phoneInput.sendKeys(formData.get(2).get(1));
+        
+        try {
+            phoneInput.click();
+        } catch (Exception e) {
+            System.out.println("test");
+        }
+        moveElementIntoView(phoneInput);
 
         WebElement messageInput = driver.findElement(By.id("textarea"));
+        System.out.println("Printing tagname " + messageInput.getTagName() + messageInput.isDisplayed());
         messageInput.clear();
         messageInput.sendKeys(formData.get(3).get(1));
+        wait.until(ExpectedConditions.elementToBeClickable(messageInput)).click();
     }
 
     @When("I click on the refresh button")
     public void i_click_on_the_refresh_button() {
-        WebElement refreshButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#blog-pager > a")));
+        WebElement refreshButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"blog-pager\"]/a")));
         refreshButton.click();
     }
 
     @Then("the form fields should be empty")
     public void the_form_fields_should_be_empty() {
-        WebElement nameInput = driver.findElement(By.id("name"));
-        WebElement emailInput = driver.findElement(By.id("email"));
-        WebElement phoneInput = driver.findElement(By.id("phone"));
+        WebElement nameInput = driver.findElement(By.xpath("//*[@id=\"name\"]"));
+        WebElement emailInput = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        WebElement phoneInput = driver.findElement(By.xpath("//*[@id=\"phone\"]"));
         WebElement messageInput = driver.findElement(By.id("textarea"));
 
         assertTrue(nameInput.getText().isEmpty());
@@ -89,6 +159,7 @@ public class StepDefinitions {
 
     @After
     public void tearDown() {
+       
         if (driver != null) {
             driver.quit();
         }
---
+++ serenity-cucumber-starter/bin/test/features/search/test_by_bebda.feature
@@ -0,0 +1,13 @@
+Feature: testname tesd 
+
+  @green1
+  Scenario: Searching for 'green' dsadadsa
+    Given Sergey is researching things on the internet
+    When he looks up "green"
+    Then he should see information about "green"
+
+  @red2
+  Scenario: Searching for 'red' dasd
+    Given Sergey is researching things on the internet
+    When he looks up "red"
+    Then he should see information about "red"
